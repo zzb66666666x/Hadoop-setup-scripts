dc7b01c05fcb5ed6e8b427b25fbec7f32ec331f8
zzb66666666x <18767066589@163.com>
2023-03-20 17:49:16 -0500

    add hadoop env setup

---
+++ setup_hadoop_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo rm /etc/profile.d/export_hadoop_env.sh
+sudo touch /etc/profile.d/export_hadoop_env.sh
+
+sudo echo "# export hadoop env variables
+export HADOOP_HOME=/opt/hadoop-3.3.4
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin" > /etc/profile.d/export_hadoop_env.sh
+
+source /etc/profile
