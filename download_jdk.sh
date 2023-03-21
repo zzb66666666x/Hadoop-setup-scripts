6e77ea2e5a13645321ef84d846be9fe550f1e66d
zzb66666666x <18767066589@163.com>
2023-03-21 00:17:43 -0500

    enforce java8

--- download_jdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-if [ ! -d "~/Downloads" ]; then
-    mkdir ~/Downloads
-fi
-cd Downloads
-
-# don't do anything unless we want to replace the current jdk version...
--- setup_java.sh
+++ setup_java.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo echo "JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.362.b09-2.el8_7.x86_64" >> /etc/environment
+cd ~/Downloads
+sudo rpm -ivh jdk-8u202-linux-x64.rpm 
+sudo echo "JAVA_HOME=/usr/java/default" > /etc/environment
