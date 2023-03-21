a53909443613725de863822b632f8b6f49f9c797
zzb66666666x <18767066589@163.com>
2023-03-20 20:01:21 -0500

    change export command in setup hadoop env

--- setup_hadoop_env.sh
+++ setup_hadoop_env.sh
@@ -2,6 +2,5 @@
 sudo rm -f /etc/profile.d/export_hadoop_env.sh
 sudo touch /etc/profile.d/export_hadoop_env.sh
 sudo echo "# export hadoop env variables
-HADOOP_HOME=/opt/hadoop-3.3.4
-export \$HADOOP_HOME
+export HADOOP_HOME=/opt/hadoop-3.3.4
 export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" > /etc/profile.d/export_hadoop_env.sh
